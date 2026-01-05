856b4343290ec61960cd66e35b76fb482821882f
unknown <power6120@namver.com>
2026-01-05 21:09:36 +0900

    gtag 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/content',
-    '@nuxtjs/sitemap'
+    '@nuxtjs/sitemap',
   ],
 
   css: ['~/assets/css/main.css'],
@@ -27,6 +27,18 @@ export default defineNuxtConfig({
           src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7892198097991803',
           async: true,
           crossorigin: 'anonymous'
+        },
+        {
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-ZZ4KPRB1WV',
+          async: true
+        },
+        {
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZZ4KPRB1WV');
+          `
         }
       ],
     }
